f2985c37e584ff914cc59155056fdaa90ac07ff0
kzmr <kurai@4travel.jp>
2014-06-03 12:06:58 +0900

    add model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    note = Notes.get_note(1)
+    Rails.logger.debug '############################'
+    Rails.logger.debug note.id
+    Rails.logger.debug note.title
+    Rails.logger.debug '############################'
   end
 end
