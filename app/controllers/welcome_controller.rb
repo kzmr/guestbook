fcb621a424b1c5903f294437df2fd108b4fe37f8
kzmr <kurai@4travel.jp>
2014-06-12 18:25:41 +0900

    a

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    #note = Notes.get_note(1)
+    note = Notes.get_note(1)
     Rails.logger.debug '############################'
-    #Rails.logger.debug note.id
-    #Rails.logger.debug note.title
+    Rails.logger.debug note.id
+    Rails.logger.debug note.title
     Rails.logger.debug '############################'
   end
 end
